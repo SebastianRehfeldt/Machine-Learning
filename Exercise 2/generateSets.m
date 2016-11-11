164aa890609f854809c43024481ba9f4281be17f
ipetronilho <ilp@student.dei.uc.pt>
2016-11-11 00:22:45 +0000

    Generate Datasets
    small changes in build_sets: it now returns the transpose of the training/testing sets and targets
    run generateSets to generate 3 different datasets.

---
+++ Exercise 2/generateSets.m
@@ -0,0 +1,11 @@
+dataset = load('54802.mat');
+TRG = dataset.Trg;
+
+% typical trainingset -> 70% seizures, with nº seizure points = nº interictal points
+[trainingSet, trainingTargetMatrix, testSet, testTargetMatrix] = build_sets(0.7, 1);
+
+% only 10% seizures on trainingset, nº seizure points = nº interictal points
+[trainingSet2, trainingTargetMatrix2, testSet2, testTargetMatrix2] = build_sets(0.1, 1);
+
+% 70% seizures, nº interictal points is 3x larger than the seizure points
+[trainingSet3, trainingTargetMatrix3, testSet3, testTargetMatrix3] = build_sets(0.7, 3);
